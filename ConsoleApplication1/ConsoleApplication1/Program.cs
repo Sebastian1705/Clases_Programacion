1702d1d4c30f16d3589d00fa3162fdce91fa4d4d
Sebastian1705 <sebaaguirre2012@gmail.com>
2018-07-04 20:44:15 -0300

    algo de c#

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona auxPersona = new Persona();
+            int edad;
+            string nombre;
+            edad = int.Parse(Console.ReadLine());
+            nombre = Console.ReadLine();
+            //nombre = auxPersona.getNombre();
+            Console.WriteLine("El nombre es " + nombre);
+            Console.WriteLine("La edad es " + edad);
+            Console.Read();
+        }
+    }
+}
