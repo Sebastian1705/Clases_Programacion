783ee5e4db4c2297ea28e9f5bcc29cc244380557
Sebastian1705 <sebaaguirre2012@gmail.com>
2018-07-05 12:31:03 -0300

    nuevo

--- Practica_2018/Alquiler/Vista.c
+++ Practica_2018/Alquiler/Vista.c
@@ -129,9 +129,9 @@ static void opcionModificacionCliente()
         {
             printf("El id es invalido\n");
         }
-        else(   !val_getString(nombre, "\nIngrese el nombre: ", "Error\n",2,50) &&
-                !val_getString(apellido, "\nIngrese el apellido: ", "Error\n",2,50) &&
-                !val_getDni(dni, "\nIngrese el dni: ", "Error\n",2,50));
+        else(   !val_getDni(dni, "\nIngrese el dni: ", "Error\n",2,50) &&
+                !val_getString(nombre, "\nIngrese el nombre: ", "Error\n",2,50) &&
+                !val_getString(apellido, "\nIngrese el apellido: ", "Error\n",2,50));
         {
             cont_modificarCliente(id,nombre,apellido,dni);
         }
