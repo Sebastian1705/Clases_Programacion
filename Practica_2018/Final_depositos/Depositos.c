3e96eca7b21bae4da27315605cf3a62c59af3665
Sebastian1705 <sebaaguirre2012@gmail.com>
2018-07-19 20:53:17 -0300

    ordenando carpetas

---
+++ Practica_2018/Final_depositos/Depositos.c
@@ -0,0 +1,148 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "ArrayList.h"
+#include "Depositos.h"
+
+Depositos* depositos_new(int producto,char* descripcion,int cantidad)
+{
+	Depositos* this = malloc(sizeof(Depositos));
+
+	if(this != NULL)
+	{
+
+		depositos_setProducto(this,producto);
+		depositos_setDescripcion(this,descripcion);
+		depositos_setCantidad(this,cantidad);
+	}
+	return this;
+}
+
+void depositos_delete(Depositos* this)
+{
+	free(this);
+}
+
+int depositos_setProducto(Depositos* this,int producto)
+{
+	this->producto = producto;
+	return 0;
+}
+
+int depositos_setDescripcion(Depositos* this,char* descripcion)
+{
+	strcpy(this->descripcion,descripcion);
+	return 0;
+}
+
+int depositos_setCantidad(Depositos* this,int cantidad)
+{
+	this->cantidad = cantidad;
+	return 0;
+}
+
+int depositos_getProducto(Depositos* this)
+{
+	return this->producto;
+}
+
+char* depositos_getDescripcion(Depositos* this)
+{
+	return this->descripcion;
+}
+
+int depositos_getCantidad(Depositos* this)
+{
+	return this->cantidad;
+}
+
+Depositos* depositos_findByProducto(ArrayList* pArray,int producto)
+{
+
+	int i;
+	Depositos* aux;
+	Depositos* retorno=NULL;
+	for(i=0;i<al_len(pArray);i++)
+	{
+		aux = al_get(pArray,i);
+		if(producto == depositos_getProducto(aux))
+		{
+			retorno = aux;
+			break;
+		}
+	}
+	return retorno;
+}
+
+Depositos* depositos_findByDescripcion(ArrayList* pArray,char* descripcion)
+{
+
+	int i;
+	Depositos* aux;
+	Depositos* retorno=NULL;
+	for(i=0;i<al_len(pArray);i++)
+	{
+		aux = al_get(pArray,i);
+		if(strcmp(descripcion,depositos_getDescripcion(aux))==0)
+		{
+			retorno = aux;
+			break;
+		}
+	}
+	return retorno;
+}
+
+Depositos* depositos_findByCantidad(ArrayList* pArray,int cantidad)
+{
+
+	int i;
+	Depositos* aux;
+	Depositos* retorno=NULL;
+	for(i=0;i<al_len(pArray);i++)
+	{
+		aux = al_get(pArray,i);
+		if(cantidad == depositos_getCantidad(aux))
+		{
+			retorno = aux;
+			break;
+		}
+	}
+	return retorno;
+}
+
+int depositos_compareByProducto(void* pA ,void* pB)
+{
+
+	int retorno = 0;
+
+	if(depositos_getProducto(pA) > depositos_getProducto(pB))
+		retorno = 1;
+	else if(depositos_getProducto(pA) < depositos_getProducto(pB))
+		retorno = -1;
+
+	return retorno;
+}
+
+int depositos_compareByDescripcion(void* pA ,void* pB)
+{
+
+	int retorno;
+
+	retorno = strcmp(depositos_getDescripcion(pA),depositos_getDescripcion(pB));
+
+	return retorno;
+}
+
+int depositos_compareByCantidad(void* pA ,void* pB)
+{
+
+	int retorno = 0;
+
+	if(depositos_getCantidad(pA) > depositos_getCantidad(pB))
+		retorno = 1;
+	else if(depositos_getCantidad(pA) < depositos_getCantidad(pB))
+		retorno = -1;
+
+	return retorno;
+}
+
---
+++ Practica_2018/Final_depositos/Depositos.h
@@ -0,0 +1,28 @@
+#include "ArrayList.h"
+
+#ifndef _DEPOSITOS_H
+#define _DEPOSITOS_H
+typedef struct
+{
+	int producto;
+	char descripcion[256];
+	int cantidad;
+}Depositos;
+#endif// _DEPOSITOS_H
+
+Depositos* depositos_new(int producto,char* descripcion,int cantidad);
+void depositos_delete(Depositos* this);
+int depositos_setProducto(Depositos* this,int producto);
+int depositos_setDescripcion(Depositos* this,char* descripcion);
+int depositos_setCantidad(Depositos* this,int cantidad);
+int depositos_getProducto(Depositos* this);
+char* depositos_getDescripcion(Depositos* this);
+int depositos_getCantidad(Depositos* this);
+Depositos* depositos_findByProducto(ArrayList* pArray,int producto);
+Depositos* depositos_findByDescripcion(ArrayList* pArray,char* descripcion);
+Depositos* depositos_findByCantidad(ArrayList* pArray,int cantidad);
+int depositos_compareByProducto(void* pA ,void* pB);
+int depositos_compareByDescripcion(void* pA ,void* pB);
+int depositos_compareByCantidad(void* pA ,void* pB);
+
+
